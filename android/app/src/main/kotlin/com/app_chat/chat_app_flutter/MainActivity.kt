1bad7716732bb85a87ccb667734fcb5670ed1d40
DhorajiyaNency <dhorajiyanency@gmail.com>
2023-02-23 16:20:59 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/app_chat/chat_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app_chat.chat_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:chat_app_flutter/firebase_options.dart';
+import 'package:chat_app_flutter/screens/splash_screen.dart';
+import 'package:chat_app_flutter/utils/app_string.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // title: 'Flutter Demo',
+      title: AppString.appName,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
